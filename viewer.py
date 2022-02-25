65536470a18d4dcf71872d786d4b91cea872f6f9
Massimiliano Iacono <massimiliano.iacono@iit.it>
2022-02-25 16:41:47 +0100

    Bounding boxes now know if mouse is over them

--- viewer.py
+++ viewer.py
@@ -38,6 +38,8 @@ import os
 
 
 class BoundingBox(Widget):
+    mouse_over = BooleanProperty(False)
+
     def __init__(self, bb_color, x, y, width, height, **kwargs):
         super(BoundingBox, self).__init__(**kwargs)
         self.x = x
@@ -45,6 +47,12 @@ class BoundingBox(Widget):
         self.width = width
         self.height = height
         self.bb_color = bb_color
+        Window.bind(mouse_pos=self.on_mouse_pos)
+
+    def on_mouse_pos(self, window, pos):
+        win_coords = self.to_window(*self.pos)
+        self.mouse_over = win_coords[0] < pos[0] < win_coords[0] + self.size[0] and\
+                          win_coords[1] < pos[1] < win_coords[1] + self.size[1]
 
 
 class LabeledBoundingBox(BoundingBox):
