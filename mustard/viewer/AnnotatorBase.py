82a6395a12c50490dbb201bb191de074b8fe69b7
Massimiliano Iacono <massimiliano.iacono@iit.it>
2025-03-25 15:54:20 +0100

    Delegating importer class to handle undo

--- mustard/viewer/AnnotatorBase.py
+++ mustard/viewer/AnnotatorBase.py
@@ -17,8 +17,6 @@ class AnnotatorBase(EventDispatcher):
         self.annotating = False
         self.label = 0
         self.initial_mouse_pos = None
-        self.previous_data_dicts = []
-        self.history_idx = 0
         self.auto_save_path = home = os.path.join(Path.home(), '.mustard')
         if not os.path.exists(self.auto_save_path):
             os.makedirs(self.auto_save_path)
@@ -34,7 +32,6 @@ class AnnotatorBase(EventDispatcher):
         raise NotImplementedError
 
     def start_annotation(self, current_time, mouse_pos, time_window):
-        # self.update_previous_dicts() #TODO restore history update 
         self.current_time = current_time
         self.initial_mouse_pos = mouse_pos
         updated_data = self.data_dict.get_data_at_time(current_time, time_window)
@@ -45,48 +42,21 @@ class AnnotatorBase(EventDispatcher):
         self.initial_data = deepcopy(self.updated_data)
         self.annotating = True
 
-    def update_previous_dicts(self):
-        self.previous_data_dicts = self.previous_data_dicts[:self.history_idx]
-        self.previous_data_dicts.append(self.data_dict.copy())
-        self.history_idx += 1
-
     def get_time_of_next_annotated_frame(self, current_time, backward=False):
         return float(self.data_dict.get_time_of_next_data_point(current_time, backward))
 
     def undo(self):
-        if self.history_idx == len(self.previous_data_dicts):
-            self.previous_data_dicts.append(self.data_dict.copy())
-        if self.history_idx < 1:
-            return
-        self.history_idx -= 1
-        self.visualizer.set_data(self.previous_data_dicts[self.history_idx])
-        self.data_dict = self.visualizer.get_data()
-        
+        self.data_dict.undo()
+        self.update_instructions()
+
     def redo(self):
-        if self.history_idx >= (len(self.previous_data_dicts) - 1):
-            return
-        self.history_idx += 1
-        self.visualizer.set_data(self.previous_data_dicts[self.history_idx])
-        self.data_dict = self.visualizer.get_data()
+        self.data_dict.redo()
+        self.update_instructions()
 
     def delete_by_time(self, time):
-        data_dict = self.data_dict
-        idx = np.searchsorted(data_dict['ts'], time)
-        if abs(data_dict['ts'][idx] - time) > 0.03:
-            return
-        self.delete_by_index(idx)
-
-    def delete_by_index(self, idx):
-        data_dict = self.data_dict
-        self.update_previous_dicts()
-        for d in data_dict:
-            if not hasattr(data_dict[d], '__len__'):
-                continue
-            try:
-                data_dict[d] = np.delete(data_dict[d], idx)
-            except IndexError:
-                pass
-    
+        self.data_dict.delete_by_time(time)
+        self.update_instructions()
+        
     def save(self, path, **kwargs):
         raise NotImplementedError
 
@@ -94,6 +64,7 @@ class AnnotatorBase(EventDispatcher):
         raise NotImplementedError
 
     def stop_annotation(self):
+        self.data_dict.update_history()
         self.update_instructions()
         self.annotating = False
         self.save(os.path.join(self.auto_save_path, 'tmp'))
